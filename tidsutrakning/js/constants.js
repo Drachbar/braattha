feb25c97de17139ab57fcaea08b9680ca76a9bfa
Mattias <drachbar@gmail.com>
2024-03-12 11:06:53 +0100

    WIP - Lägg till flera tabeller
    Lyssnare läggs till på existerande tabeller också tyvärr.

---
+++ tidsutrakning/js/constants.js
@@ -0,0 +1,48 @@
+const tabell = `
+<section class="grid">
+    <section>
+        <h2>Måndag</h2>
+        <input type="time" />
+        <input type="time" />
+        <input type="time" />
+        <input type="time" />
+        <p class="sum"></p>
+    </section>
+
+    <section>
+        <h2>Tisdag</h2>
+        <input type="time" />
+        <input type="time" />
+        <input type="time" />
+        <input type="time" />
+        <p class="sum"></p>
+    </section>
+
+    <section>
+        <h2>Onsdag</h2>
+        <input type="time" />
+        <input type="time" />
+        <input type="time" />
+        <input type="time" />
+        <p class="sum"></p>
+    </section>
+
+    <section>
+        <h2>Torsdag</h2>
+        <input type="time" />
+        <input type="time" />
+        <input type="time" />
+        <input type="time" />
+        <p class="sum"></p>
+    </section>
+
+    <section>
+        <h2>Fredag</h2>
+        <input type="time" />
+        <input type="time" />
+        <input type="time" />
+        <input type="time" />
+        <p class="sum"></p>
+    </section>
+</section>
+`
--- tidsutrakning/js/myscript.js
+++ tidsutrakning/js/myscript.js
@@ -1,7 +1,16 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     const inputFields = document.querySelectorAll('input[type="time"]');
+    const button = document.querySelector('button');
 
+    button.addEventListener('click', addTable);
     setEnterAsTab();
     onInputChange(inputFields, calculateTime);
     console.dir(getWeekNumber(new Date()));
 });
+
+function addTable() {
+    const button = document.querySelector('button');
+    button.insertAdjacentHTML('afterend', tabell)
+    const inputFields = document.querySelectorAll('input[type="time"]');
+    onInputChange(inputFields, calculateTime);
+}
